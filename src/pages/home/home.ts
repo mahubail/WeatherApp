9d0875717cd697d06fbb0e8d378f663b8f7b3839
mahubail <7ubail.ma@gmail.com>
2019-05-18 22:14:03 +0300

    Add Alert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+Countries=[
+  {"Country":"Bahrain","City":"Manama"},
+  {"Country":"KSA","City":"Riyadh"},
+  {"Country":"UAE","City":"Dubai"}
+ 
+];
+  constructor(public navCtrl: NavController, private alertCtrl:AlertController) {
 
   }
+  loadAbout(){
+    this.navCtrl.push(AboutPage);
+  }
 
+  getDetails(item){console.log(item.City);
+  
+  }
+
+  addLocation(){
+    let addCte = this.alertCtrl.create({
+      title:"Add City",
+      message: "Enter a new location",
+      inputs:[
+        {type:"text", name:"Country", placeholder:"Enter Country"},
+        {type:"text", name:"City", placeholder:"Enter City"}
+    ],
+    buttons:
+    [
+      { text:"Cancel"},
+      { text: "Add", handler: (newLocation)=>{this.Countries.push({"Country":newLocation.Country,"City":newLocation.City})}}
+    ]
+
+    });
+    addCte.present();
+  }
 }
